dbf9a4a6c5f79c323b6f6a25a62837f34827e07f
Scott Tang <fq600@yahoo.com>
2018-08-06 10:05:08 -0500

    done

--- application.rb
+++ application.rb
@@ -1,10 +1,8 @@
 class Application
-
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    @name = "Scott"
+    resp.write "Hello, my name is #{@name}"
     resp.finish
   end
-
 end
-
